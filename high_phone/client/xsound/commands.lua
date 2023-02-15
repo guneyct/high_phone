d04668ee9ce13a36e48ff76fe495f78ad94e177e
guneyct <120514341+guneyct@users.noreply.github.com>
2023-02-15 18:27:03 +0300

    Delete high_phone directory

--- high_phone/client/xsound/commands.lua
+++
@@ -1,9 +0,0 @@
-AddEventHandler("xsound:streamerMode", function(value)
-    disableMusic = value
-
-    if value then
-        for k, v in pairs(soundInfo) do
-            Destroy(v.id)
-        end
-    end
-end)
